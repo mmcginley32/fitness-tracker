3a85e1b0a42a78cfb76ed3c316a80d6ef4fb0bb1
Mason McGinley <masonmcginley32@gmail.com>
2020-11-01 15:14:34 -0700

    server items

---
+++ README.md
@@ -0,0 +1,41 @@
+# Workout-Tracker
+
+## Description
+
+This application is a database that simply stores your workouts. When the user loads the page, they should be given the option to create a new workout or continue with their last workout.
+
+## User Story
+
+```
+As a active person
+I want to be able to log multiple exercises in a workout on a given day. I should also be able to track the name, type, weight, sets, reps, and duration of exercise. 
+So that I can track my workouts.
+```
+
+## Demo 
+
+The following animation demonstrates the application functionality:
+
+![home](https://github.com/mmcginley/fitness-tracker/blob/main/assets/FITNESS.gif)
+
+## Links
+
+https://github.com/mmcginley/fitness-tracker
+
+
+
+## Resources 
+
+Node JS, Express, Mongo, Mongoose
+
+## Installation
+
+Packages required to run this program are: 
+
+```
+npm install
+npm install 
+npm install express
+npm install mongoose
+npm install mongo
+```
---
+++ routes/api-routes.js
@@ -0,0 +1,50 @@
+const router = require("express").Router();
+const Workout = require("../models/Workout.js");
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+      params.id, 
+      { $push: {exercises: body}},
+      { new: true, runValidators: true}
+  )
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({}).limit(7)
+      .then(dbWorkout => {
+        console.log(dbWorkout);
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+
+module.exports = router;
